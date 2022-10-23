9d91503e8784e24951b548b849b8845d98df2431
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2022-10-22 23:13:16 -0300

    Desafio renato 21/10 e aulas

---
+++ diretivas/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Produto } from './interfaces/Produto';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   title = 'diretivas';
+  // p: Produto = {
+  //   nome: 'Xbox',
+  //   descricao: 'Console da Microsoft',
+  //   estoque: 250,
+  //   preco: 4000,
+  //   imagem: 'https://images.unsplash.com/photo-1597872200969-2b65d56bd16b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  // }
+
+  nome: string = ''
+  imagem: string = ''
+  preco: number = 0
+  estoque: number = 0
+  descricao: string = ''
+
+
+  produtos: Produto[] = []
+
+  adicionarProduto(evento: any): void {
+    console.log(evento)
+    evento.preventDefault()
+    evento.stopPropagation()
+
+    this.produtos.push({
+      descricao: this.descricao,
+      imagem: this.imagem,
+      preco: this.preco,
+      estoque: this.estoque,
+      nome: this.nome
+    })
+  }
+
+  excluirProduto(indexExclusao: String){
+    for (const iterator of this.produtos) {
+      if(iterator.nome == indexExclusao){
+        const x = this.produtos.indexOf(iterator)
+        this.produtos.splice(x, 1)
+      }
+    }
+  }
+
+}
---
+++ diretivas/src/app/produto-card/produto-card.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Produto } from '../interfaces/Produto';
+
+@Component({
+  selector: 'app-produto-card',
+  templateUrl: './produto-card.component.html',
+  styleUrls: ['./produto-card.component.css']
+})
+export class ProdutoCardComponent implements OnInit {
+
+  @Input()
+  prod: Produto = {
+    descricao: '',
+    estoque: 0,
+    imagem: '',
+    nome: '',
+    preco: 0
+  }
+
+  @Output() excluindoEvent = new EventEmitter<String>();
+
+  excluir(indexExclusao: String){
+    this.excluindoEvent.emit(indexExclusao)
+  }
+
+  mostrarProduto: boolean = true
+
+  esconderProduto(): void {
+    if(this.mostrarProduto){
+      this.mostrarProduto = false
+    } else {
+      this.mostrarProduto = true
+    }
+  }
+
+  // showProduto(): void {
+  //   if(!this.mostrarProduto) {
+  //     this.mostrarProduto = true
+  //     }
+  // }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
