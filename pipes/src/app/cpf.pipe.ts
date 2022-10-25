4130ef9225aa3129d702b3baf8d62da7540e0e43
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2022-10-25 17:51:31 -0300

    Aula form template Driven 25/10

--- pipes/src/app/cpf.pipe.ts
+++ pipes/src/app/cpf.pipe.ts
@@ -7,6 +7,7 @@ export class CpfPipe implements PipeTransform {
 
   transform(value: String): any {
     return value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '\$1.\$2.\$3\-\$4');
+    // solucao renato. (/([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/, '$1.$2.$3-$4');
   }
 
 }
