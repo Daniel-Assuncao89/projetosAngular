e86fc21a7040118af253f59e9c8fbe575c5c529e
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2022-11-03 17:46:59 -0300

    Aula 01/11 e Desafio parcialmente concluido

--- bc25-github-app/src/app/app.component.ts
+++ bc25-github-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GithubRepos, GithubUser } from './interfaces/githubUser';
+import { GithubApiService } from './services/github-api.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,25 @@ export class AppComponent {
     username: ['', [ Validators.required ]]
   })
 
+  gUser!: GithubUser
+  gRepo!: GithubRepos
+
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private githubService: GithubApiService
   ) {}
+
+  procurar(){
+    const username = this.githubForm.get('username')?.value // recuperando o valor.
+    //console.log(username)
+    this.githubService.procurarUsuario(username).subscribe((user) => {
+      //console.log(user)
+     // console.log(`O usuario ${user.login} tem ${user.followers} seguidores`)
+     this.gUser = user
+     this.githubService.procurarRepo(username).subscribe((repo) => {
+      this.gRepo = repo
+    })
+    }
+    )
+  }
 }
---
+++ bc25-github-app/src/app/services/github-api.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { GithubRepos, GithubUser } from '../interfaces/githubUser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubApiService {
+
+  private readonly baseURL: string = 'https://api.github.com/users/'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /**
+   * Métodos HTTP:
+   *  - Leitura de dados: GET
+   *  - Criação de dados: POST
+   *  - Atualização de dados: PUT
+   *  - Deleção de dados: DELETE
+   */
+  procurarUsuario(username: string) {
+    /**
+     * o objeto http, responsavel por fazer as requisições, possui o metodo get(), que serve para fazer requisições HTTP utilizando o verbo GET. Basta apenas passar a URL de acesso como parametro
+     */
+    return this.http.get<GithubUser>(this.baseURL + username) // retorna um observable
+  }
+
+  procurarRepo(username:string) {
+    return this.http.get<GithubRepos>(this.baseURL + username + "/repos")
+  }
+}
